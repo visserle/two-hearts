f4e40a8dfb743c07413284b50c1abb940d1bd04f
thev-visserd <thanks@mailbox.org>
2022-04-08 14:10:51 +0200

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,36 +1,26 @@
 # Create lists for iteration
 
-list_str = ['01','02','03','04','05','06','07','08','09'] + list(map(str, range(10, 70)))
+list_str = ['01','02','03','04']#,'05','06','07','08','09'] + list(map(str, range(10, 31)))
 
-list_datafiles_raw = 'sample-data/raw/00{list_str}'
-list_datafiles_1k = 'sample-data/1k/00{list_str}'
-list_datafiles = 'sample-data/00{list_str}'
+list_datafiles_raw_SIT = 'data/ecg_raw/SIT/ecg_{list_str}'
+list_datafiles_raw_GAZE = 'data/ecg_raw/GAZE/ecg_{list_str}'
 
 append_type = '.npy'
 append_google = 'drive/MyDrive/Masterarbeit/Code/two-hearts/'
 
-datafiles_wfdb = []
+datafiles_raw_SIT = []
 for i in range(len(list_str)):
-    datafiles_str = list_datafiles_raw.format(list_str = list_str[i])
-    datafiles_wfdb.append(datafiles_str)
+    datafiles_str = list_datafiles_raw_SIT.format(list_str = list_str[i])
+    datafiles_raw_SIT.append(datafiles_str)
+datafiles_raw_SIT = [sub + append_type for sub in datafiles_raw_SIT]
 
-datafiles_raw = []
+datafiles_raw_GAZE = []
 for i in range(len(list_str)):
-    datafiles_str = list_datafiles_raw.format(list_str = list_str[i])
-    datafiles_raw.append(datafiles_str)
-datafiles_raw = [sub + append_type for sub in datafiles_raw]
+    datafiles_str = list_datafiles_raw_GAZE.format(list_str = list_str[i])
+    datafiles_raw_GAZE.append(datafiles_str)
+datafiles_raw_GAZE = [sub + append_type for sub in datafiles_raw_GAZE]	
 
-datafiles_1k = []
-for i in range(len(list_str)):
-    datafiles_str = list_datafiles_1k.format(list_str = list_str[i])
-    datafiles_1k.append(datafiles_str)
-datafiles_1k = [sub + append_type for sub in datafiles_1k]
 
-datafiles = []
-for i in range(len(list_str)):
-    datafiles_str = list_datafiles.format(list_str = list_str[i])
-    datafiles.append(datafiles_str)
-datafiles = [sub + append_type for sub in datafiles]
+datafiles_raw_SIT_google = [append_google + sub for sub in datafiles_raw_SIT]
+datafiles_raw_GAZE_google = [append_google + sub for sub in datafiles_raw_GAZE]
 
-datafiles_raw_google = [append_google + sub for sub in datafiles_raw]
-datafiles_google = [append_google + sub for sub in datafiles]
